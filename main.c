645a41382f13485c0851d7a0d144e389f4977f2c
Viktor Hoffmann <st177556@stud.uni-stuttgart.de>
2023-08-15 17:33:49 +0200

    fixed y

--- main.c
+++ main.c
@@ -13,8 +13,8 @@ void gpsToNed(float lat1, float long1, float alt1, float lat2, float long2, floa
     float originLong = degreeToRad(long1);
     float currentLat = degreeToRad(lat2);
     float currentLong = degreeToRad(long2);
-    float tempLat = originLat;
-    float tempLong = originLong;
+    float projectedLat = originLat;
+    float projectedLong = originLong;
 
     int earthRadius = 6371000; //define Earth Radius in m
 
@@ -22,20 +22,14 @@ void gpsToNed(float lat1, float long1, float alt1, float lat2, float long2, floa
     float diffLat = 0;      //calc difference in radians between two latitudes and longitudes
     float diffLong = (currentLong-originLong);
 
-    float a = sin(diffLat/2) * sin(diffLong/2) + sin(diffLong/2) * sin(diffLong/2) * cos(originLat) * cos(tempLat); //Haversine Formula
-    float c = 2 * atan2(sqrt(a), sqrt(1-a));
-    float x = earthRadius * c;
+    float a = sin(diffLat/2) * sin(diffLat/2) + sin(diffLong/2) * sin(diffLong/2) * cos(originLat) * cos(projectedLat); //Haversine Formula
+    float x = earthRadius * (2 * atan2(sqrt(a), sqrt(1-a)));
 
-    //Now we calculate direct distance
-    diffLat = (currentLat-originLat);      //calc difference in radians between two latitudes and longitudes
-    diffLong = (currentLong-originLong);
-
-    a = sin(diffLat/2) * sin(diffLong/2) + sin(diffLong/2) * sin(diffLong/2) * cos(originLat) * cos(currentLat); //Haversine Formula
-    c = 2 * atan2(sqrt(a), sqrt(1-a));
-    float d = earthRadius * c;
-
-    //Finding missing side of triangle
-    float y = sqrt((d*d) - (x*x));
+    //next we calculate latitude distance by assuming origin and current have same longitude
+    diffLat = (currentLat-originLat);
+    diffLong = 0;
+    a = sin(diffLat/2) * sin(diffLat/2) + sin(diffLong/2) * sin(diffLong/2) * cos(originLat) * cos(currentLat); //Haversine Formula
+    float y = earthRadius * (2 * atan2(sqrt(a), sqrt(1-a)));
 
     //As the Earth is flat and not a donut altitude corresponds directly to y coordinates (Negative because of NED)
     float z = -(alt2-alt1);
